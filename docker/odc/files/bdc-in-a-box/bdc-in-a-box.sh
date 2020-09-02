5459787893dbc34b9cb7279be8b6c62edae05893
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-09-01 21:16:38 -0300

    remove branch change

--- docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
+++ docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install stac2odc
-git clone https://github.com/brazil-data-cube/bdc-odc.git 
-cd bdc-odc && git checkout bdc-env && git branch bdc-env -u origin/bdc-env
+git clone https://github.com/brazil-data-cube/bdc-odc.git && cd bdc-odc
 pip3 install  git+https://github.com/brazil-data-cube/stac.py
 cd stac2odc && python3 setup.py install && cd ../.. && rm -rf bdc-odc
 
