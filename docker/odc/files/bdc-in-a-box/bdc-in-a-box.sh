13b36c7585c345b26bfe2dc85cd59552f1187a37
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-09-16 17:04:53 -0300

    update bdc in a box
    fix some erros in bdc-in-a-box and auto-install in odc-core image

--- docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
+++ docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # Indexing
 while IFS=, read -r COLLECTIONID PLATAFORM SENSOR MDTYPE LIMIT ADVANCEDFILTER
 do
     mkdir -p /data/products/${COLLECTIONID}/datasets/data
     stac2odc collection2product -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID}/${COLLECTIONID}.yaml --units m -p "${PLATFORM}" --instrument "${SENSOR}" --type "${MDTYPE}"
     datacube product add /data/products/${COLLECTIONID}/${COLLECTIONID}.yaml
-    stac2odc item2dataset -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID}/datasets/ --units m -p "${PLATFORM}"
+    stac2odc item2dataset -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID}/datasets/ --units m -p "${PLATFORM}" \
                   --instrument "${SENSOR}" -m "${LIMIT}" --download --download-out /data/products/${COLLECTIONID}/datasets/data \
                   --advanced-filter "${ADVANCEDFILTER}"
 
--- docker/odc/files/install_stac2odc.sh
+++ docker/odc/files/install_stac2odc.sh
@@ -4,5 +4,4 @@ echo "Installing stac2odc"
 
 # Install stac2odc
 git clone https://github.com/brazil-data-cube/bdc-odc.git && cd bdc-odc
-pip3 install  git+https://github.com/brazil-data-cube/stac.py
 cd stac2odc && python3 setup.py install && cd ../.. && rm -rf bdc-odc
