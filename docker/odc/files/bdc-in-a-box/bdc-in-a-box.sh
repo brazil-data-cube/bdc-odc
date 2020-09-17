789bbcbf61cce046b44eb2bd0f1645d3702fa8fd
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-09-17 12:00:48 -0300

    Update bdc in a box tool

--- docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
+++ docker/odc/files/bdc-in-a-box/bdc-in-a-box.sh
@@ -5,12 +5,18 @@ set -e
 # Indexing
 while IFS=, read -r COLLECTIONID PLATAFORM SENSOR MDTYPE LIMIT ADVANCEDFILTER
 do
-    mkdir -p /data/products/${COLLECTIONID}/datasets/data
-    stac2odc collection2product -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID}/${COLLECTIONID}.yaml --units m -p "${PLATFORM}" --instrument "${SENSOR}" --type "${MDTYPE}"
-    datacube product add /data/products/${COLLECTIONID}/${COLLECTIONID}.yaml
-    stac2odc item2dataset -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID}/datasets/ --units m -p "${PLATFORM}" \
-                  --instrument "${SENSOR}" -m "${LIMIT}" --download --download-out /data/products/${COLLECTIONID}/datasets/data \
+    COLLECTIONID_REPLACE=$(echo $COLLECTIONID | sed -e "s/-/"_"/g")
+
+    {
+    mkdir -p /data/products/${COLLECTIONID_REPLACE}/datasets/data
+    stac2odc collection2product -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID_REPLACE}/${COLLECTIONID_REPLACE}.yaml --units m -p "${PLATFORM}" --instrument "${SENSOR}" --type "${MDTYPE}"
+    datacube product add /data/products/${COLLECTIONID_REPLACE}/${COLLECTIONID_REPLACE}.yaml
+    stac2odc item2dataset -c "${COLLECTIONID}" -o /data/products/${COLLECTIONID_REPLACE}/datasets/ --units m -p "${PLATFORM}" \
+                  --instrument "${SENSOR}" -m "${LIMIT}" --download --download-out /data/products/${COLLECTIONID_REPLACE}/datasets/data \
                   --advanced-filter "${ADVANCEDFILTER}"
 
-    find /data/products/${COLLECTIONID}/datasets/*.yaml -exec datacube -vvv dataset add -p ${COLLECTIONID} {} \;
+    find /data/products/${COLLECTIONID_REPLACE}/datasets/*.yaml -exec datacube -vvv dataset add -p ${COLLECTIONID_REPLACE} {} \;
+    } || {
+      echo "No Image found for ${COLLECTIONID}". Check your filter
+    }
 done <collections.list
