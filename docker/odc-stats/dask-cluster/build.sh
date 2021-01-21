cc6e2cb5a3b33eb9e58d9b668c8564ce390853e1
Felipe Carlos <felipe.carlos@fatec.sp.gov.br>
2021-01-21 10:13:02 -0300

    remove docker directory and stac2odc

--- docker/odc-stats/dask-cluster/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set -e
-docker-compose stop && docker-compose rm -f && docker-compose build --no-cache --parallel --force-rm && docker-compose up -d
--- docker/odc/files/install_stac2odc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing stac2odc"
-
-# Install stac2odc
-git clone https://github.com/brazil-data-cube/bdc-odc.git && cd bdc-odc
-cd stac2odc && python3 setup.py install && cd ../.. && rm -rf bdc-odc
